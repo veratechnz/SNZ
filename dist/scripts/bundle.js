4a5701df74f897604cd6dfa3a936708bf0bc91d5
Jared Neems <jared.neems@stats.govt.nz>
2016-10-03 11:49:46 +1300

    10-03-16

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -9762,12 +9762,21 @@ var materialize = require("materialize");
 
 
 
-//All  Custom - JS
-(function(){
+//ALL CUSTOM JS
 
+// Created: Sep 30, 2016 
+// Author: Jared Neems // Statistics New Zealand
+// Pupose: Front End UI Javascript For Initial Design, User Interface, Responsive
+// 		   Testing and Presentation.
+// Standards: 
+
+(function(){
+	console.log('obj');
 	// All UI updates and adjustments
 	var Ui = {
+
 	 	checkScreen:  function(){
+	 		console.log('z');
 	 		//Check window size and run internal functions. 
 	 		var checkSize = $(window).width();
 	 		var oldNav = 'leftNavContainer';
@@ -9775,13 +9784,17 @@ var materialize = require("materialize");
 
 	 		//Removes static desktop side nav
 			function removeNav(){
+				console.log('zb');
 				var elem = document.getElementById(oldNav);
 				elem.parentNode.removeChild(elem);
 			}
 
 			//Changes Material design class to full width
 			function updateMatClass(){
+				//Change Materilize layout from 10 to 12 for responsiv mobile. Checking via regex.
 				document.getElementById(newBody).className = document.getElementById(newBody).className.replace( /(?:^|\s)s10(?!\S)/g , ' s12' );
+				//Trigger inclusion of mobile side nav
+				mobileSideNav();
 			}
 
 			//Stretches current horizontal nav to full width after side nav removal
@@ -9789,11 +9802,21 @@ var materialize = require("materialize");
 				document.getElementById("navHorizontal").style.width = "100%";
 			}
 
+			//Activate and setup mobile side navigation
+			function mobileSideNav(){
+				console.log('a');
+				$('.button-collapse').sideNav();
+				$('.side-nav').css('display', 'block');
+			}
+
 			if(checkSize <= 1024){
+				console.log('b');
 				removeNav();
 				stretchHorizontalNav();
 				updateMatClass();
 			}
+
+
 
 		}
 	};
--- src/js/main.js
+++ src/js/main.js
@@ -10,12 +10,21 @@ var materialize = require("materialize");
 
 
 
-//All  Custom - JS
-(function(){
+//ALL CUSTOM JS
 
+// Created: Sep 30, 2016 
+// Author: Jared Neems // Statistics New Zealand
+// Pupose: Front End UI Javascript For Initial Design, User Interface, Responsive
+// 		   Testing and Presentation.
+// Standards: 
+
+(function(){
+	console.log('obj');
 	// All UI updates and adjustments
 	var Ui = {
+
 	 	checkScreen:  function(){
+	 		console.log('z');
 	 		//Check window size and run internal functions. 
 	 		var checkSize = $(window).width();
 	 		var oldNav = 'leftNavContainer';
@@ -23,13 +32,17 @@ var materialize = require("materialize");
 
 	 		//Removes static desktop side nav
 			function removeNav(){
+				console.log('zb');
 				var elem = document.getElementById(oldNav);
 				elem.parentNode.removeChild(elem);
 			}
 
 			//Changes Material design class to full width
 			function updateMatClass(){
+				//Change Materilize layout from 10 to 12 for responsiv mobile. Checking via regex.
 				document.getElementById(newBody).className = document.getElementById(newBody).className.replace( /(?:^|\s)s10(?!\S)/g , ' s12' );
+				//Trigger inclusion of mobile side nav
+				mobileSideNav();
 			}
 
 			//Stretches current horizontal nav to full width after side nav removal
@@ -37,11 +50,21 @@ var materialize = require("materialize");
 				document.getElementById("navHorizontal").style.width = "100%";
 			}
 
+			//Activate and setup mobile side navigation
+			function mobileSideNav(){
+				console.log('a');
+				$('.button-collapse').sideNav();
+				$('.side-nav').css('display', 'block');
+			}
+
 			if(checkSize <= 1024){
+				console.log('b');
 				removeNav();
 				stretchHorizontalNav();
 				updateMatClass();
 			}
+
+
 
 		}
 	};
